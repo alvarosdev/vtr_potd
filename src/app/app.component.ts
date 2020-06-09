a36b9b7d46468ce86ba5a9769dae49b1592000f1
arvrsh <alvarogodoyg@gmail.com>
2020-06-09 00:32:02 -0400

    RN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import genPassOfDay from 'arrispwod';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vtrpotd';
+  date:String = new Date().toString();
+  potd:String = this.generatePotd();
+
+
+  public generatePotd(seed: string = "vtr2014"):string {
+    let date: Date = new Date();
+    return genPassOfDay(date, seed);
+  }
+
+  public refreshPotd(){
+    this.date = new Date().toString();
+    this.potd = this.generatePotd();
+    console.log("Nuevo potd: ", this.potd);
+  }
+
 }
+
+
+
